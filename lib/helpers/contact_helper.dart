7d9581f5410aa67aa9ac6219c096bc79d830d5a2
Divino Rodrigues <divino.sr@hotmail.com>
2020-05-26 20:13:20 -0300

    Salvando e Obtendo Contato

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -32,12 +32,30 @@ class ContactHelper {
 
     return await openDatabase(path, version: 1,
         onCreate: (Database db, int newerVersion) async {
-      await db.execute(
-          "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, "
+      await db
+          .execute("CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, "
               "$nameColumn TEXT, $emailColumn TEXT, $phoneColumn TEXT, "
-              "$imgColumn TEXT)"
-      );
+              "$imgColumn TEXT)");
     });
+  }
+
+  Future<Contact> saveContact(Contact contact) async {
+    Database dbContact = await db;
+    contact.id = await dbContact.insert(contactTable, contact.toMap());
+    return contact;
+  }
+
+  Future<Contact> getContact(int id) async {
+    Database dbContact = await db;
+    List<Map> maps = await dbContact.query(contactTable,
+    columns: [idColumn, nameColumn, emailColumn, phoneColumn, imgColumn],
+    where: "$idColumn = ?",
+      whereArgs: [id]);
+    if(maps.length > 0){
+      return Contact.fromMap(maps.first);
+    } else {
+      return null;
+    }
   }
 }
 
